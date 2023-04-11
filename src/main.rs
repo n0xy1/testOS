e8ca62ecae6d51d143989fa86509f9de1988e620
dyl <dyl65535@gmail.com>
2023-04-11 22:34:57 +1000

    first runthrough

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use test_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("TestOS{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_os::test_panic_handler(info)
+}
